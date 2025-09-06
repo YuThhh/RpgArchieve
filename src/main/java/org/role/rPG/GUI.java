def08ecc0049c0f24784773b5a4d132657c1ee1f
YuThhh <101546506+YuThhh@users.noreply.github.com>
2025-09-06 21:52:28 +0900

    GUI 작업 진행중

--- src/main/java/org/role/rPG/GUI.java
+++ src/main/java/org/role/rPG/GUI.java
@@ -1,7 +1,38 @@
 package org.role.rPG;
 
-public class GUI {
-  public menu() {
-    
-  }
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.jetbrains.annotations.NotNull;
+
+public class GUI implements InventoryHolder {
+
+    private final Inventory inv;
+
+    public GUI() {
+        // InventoryHolder 인터페이스의 메서드인 getInventory()만 @Override를 사용합니다.
+        Component titleComponent = Component.text("RPG 메뉴", NamedTextColor.YELLOW);
+        inv = Bukkit.createInventory(this, 54, titleComponent);
+        initializeItems();
+    }
+
+    private void initializeItems() {
+        // 아이템 추가 예시
+        // inv.setItem(슬롯, 아이템);
+        // 슬롯 번호는 0부터 시작
+        inv.setItem(0, new ItemStack(org.bukkit.Material.IRON_SWORD));
+    }
+
+    @Override
+    public @NotNull Inventory getInventory() {
+        return inv;
+    }
+
+    public void openInventory(final Player p) {
+        p.openInventory(inv);
+    }
 }
--- src/main/java/org/role/rPG/RPG.java
+++ src/main/java/org/role/rPG/RPG.java
@@ -1,17 +1,35 @@
 package org.role.rPG;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
-public final class RPG extends JavaPlugin {
+import java.util.Objects;
+
+public final class RPG extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Objects.requireNonNull(this.getCommand("메뉴")).setExecutor(this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String @NotNull [] args) {
+        if (sender instanceof Player player) {
+            // UI 클래스의 새 인스턴스 생성
+            GUI gui = new GUI();
+            // GUI 열기
+            gui.openInventory(player);
+            return true;
+        }
+        return false;
     }
 }
