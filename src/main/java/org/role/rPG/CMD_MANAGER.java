1cb3c0974939ac1da87c640935143cd5875057b2
최우준 <choiwj345300@gmail.com>
2025-09-09 19:47:48 +0900

    스탯 작업 중 크리티컬 및 크확 작업 중 관련 명령어 추가

--- src/main/java/org/role/rPG/CMD_MANAGER.java
+++ src/main/java/org/role/rPG/CMD_MANAGER.java
@@ -80,9 +80,76 @@ public class CMD_MANAGER implements CommandExecutor {
                 sender.sendMessage(mm.deserialize("<red>방어력은 숫자여야 합니다."));
             }
             return true;
+
         } else if (command.getName().equalsIgnoreCase("setcrit")) {
+            // [개선 1] 권한 확인: OP가 아니거나 "rpg.command.setdef" 권한이 없으면 거부
+            if (!sender.isOp() && !sender.hasPermission("rpg.command.setdef")) {
+                sender.sendMessage(mm.deserialize("<red>이 명령어를 사용할 권한이 없습니다."));
+                return true;
+            }
+
+            // [개선 2] 인자 개수 확인: 명령어 형식이 잘못되었으면 사용법 안내
+            if (args.length != 2) {
+                sender.sendMessage(mm.deserialize("<red>사용법: /setdef <플레이어> <수치>"));
+                return true;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage(mm.deserialize("<red>플레이어를 찾을 수 없습니다."));
+                return true;
+            }
+
+            try {
+                double crit = Double.parseDouble(args[1]);
+                PER_DATA.getInstance().setPlayerCrit(target.getUniqueId(), crit);
+
+                // [개선 3] 메시지 형식을 MiniMessage로 통일
+                String senderMessage = String.format("<green>%s님의 크리티컬을 <white>%.1f</white>로 설정했습니다.</green>", target.getName(), crit);
+                String targetMessage = String.format("<aqua>당신의 크리티컬이 <white>%.1f</white>로 설정되었습니다.</aqua>", crit);
+
+                sender.sendMessage(mm.deserialize(senderMessage));
+                target.sendMessage(mm.deserialize(targetMessage));
+
+            } catch (NumberFormatException e) {
+                sender.sendMessage(mm.deserialize("<red>크리티컬은 숫자여야 합니다."));
+            }
+            return true;
 
         } else if (command.getName().equalsIgnoreCase("setcritdmg")) {
+            // [개선 1] 권한 확인: OP가 아니거나 "rpg.command.setdef" 권한이 없으면 거부
+            if (!sender.isOp() && !sender.hasPermission("rpg.command.setdef")) {
+                sender.sendMessage(mm.deserialize("<red>이 명령어를 사용할 권한이 없습니다."));
+                return true;
+            }
+
+            // [개선 2] 인자 개수 확인: 명령어 형식이 잘못되었으면 사용법 안내
+            if (args.length != 2) {
+                sender.sendMessage(mm.deserialize("<red>사용법: /setdef <플레이어> <수치>"));
+                return true;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage(mm.deserialize("<red>플레이어를 찾을 수 없습니다."));
+                return true;
+            }
+
+            try {
+                double critdmg = Double.parseDouble(args[1]);
+                PER_DATA.getInstance().setPlayerCritDamage(target.getUniqueId(), critdmg);
+
+                // [개선 3] 메시지 형식을 MiniMessage로 통일
+                String senderMessage = String.format("<green>%s님의 크리티컬 피해를 <white>%.1f</white>로 설정했습니다.</green>", target.getName(), critdmg);
+                String targetMessage = String.format("<aqua>당신의 크리티컬 피해가 <white>%.1f</white>로 설정되었습니다.</aqua>", critdmg);
+
+                sender.sendMessage(mm.deserialize(senderMessage));
+                target.sendMessage(mm.deserialize(targetMessage));
+
+            } catch (NumberFormatException e) {
+                sender.sendMessage(mm.deserialize("<red>크리티컬 피해는 숫자여야 합니다."));
+            }
+            return true;
 
         }
 
